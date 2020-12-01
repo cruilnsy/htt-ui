93553ede5bfe7f51e0081353243a0f5d5830e8ba
Rui Cui <cruilnsy@gmail.com>
2020-11-30 18:38:34 -0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>HometownTicketing</h1>
+    <router-view/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'App'
+})
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/utils/request.ts
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+const request = axios.create({
+  // baseURL,
+  // timeoout
+})
+// request interceptor
+// response interceptor
+export default request
